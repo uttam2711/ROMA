d88de0840fb93324238e2a4a76efa1c8f0899b38
uttam kumar <prajapatuttamkumar2711@gmail.com>
2026-02-08 19:56:24 +0530

    add environment files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  GEMINI_API_KEY: "AIzaSyD0_v0s83BciPBySxfqwSYgOiQhQ8G_zUc"
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  GEMINI_API_KEY: "AIzaSyD0_v0s83BciPBySxfqwSYgOiQhQ8G_zUc"
+};
