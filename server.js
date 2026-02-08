74f0fe27c3540502d06aa4ad41deaf983a11a0b0
uttam2711 <prajapatuttamkumar2711@gmail.com>
2026-02-08 14:04:17 +0530

    Update paths and logging in server.js

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, 'dist/roma')));
+// Serve Angular dist folder
+app.use(express.static(path.join(__dirname, "dist/roma/browser")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/roma/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/roma/browser/index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log(`ROMA running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
